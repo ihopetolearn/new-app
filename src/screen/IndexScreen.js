f5646d136190824c2fe06737a906565b713460d9
paiman <arifpaiman9@gmail.com>
2024-04-14 22:06:06 -0500

    added

---
+++ src/screen/IndexScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text,View,StyleSheet } from "react-native";
+ const  IndexScreen = ()=>{
+    return(
+        <Text> paiman screen </Text>
+    );
+};
+  const styles=StyleSheet.create({});
+  export default IndexScreen;
